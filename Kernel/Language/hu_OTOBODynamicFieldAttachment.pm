7e59bfc136ec2948e581281080e6da04541d93a5
Stefan Härter <sha@rother-oss.com>
2024-03-28 11:30:26 +0100

    Updated language files via CodePolicy.

--- Kernel/Language/hu_OTOBODynamicFieldAttachment.pm
+++ Kernel/Language/hu_OTOBODynamicFieldAttachment.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -24,7 +24,7 @@ sub Data {
     my $Self = shift;
 
     # Template: AdminDynamicFieldAttachment
-    $Self->{Translation}->{'Field'} = 'Mező';
+    $Self->{Translation}->{'Field'}                         = 'Mező';
     $Self->{Translation}->{'Maximum amount of attachments'} = 'Mellékletek legnagyobb száma';
     $Self->{Translation}->{'Change this, if you need more or less attachments to be stored in this dynamic field.'} =
         'Változtassa meg, ha több vagy kevesebb mellékletet szükséges tárolni ebben a dinamikus mezőben.';
@@ -37,28 +37,27 @@ sub Data {
         'A mellékletek legnagyobb számának ennél a melléklet típusú dinamikus mezőnél pozitív számnak kell lennie.';
     $Self->{Translation}->{'The maximum attachment size for this dynamic field of type attachment must be a positive number.'} =
         'A legnagyobb mellékletméretnek ennél a melléklet típusú dinamikus mezőnél pozitív számnak kell lennie.';
-    $Self->{Translation}->{'Need ValidID!'} = 'Érvényességazonosító szükséges!';
-    $Self->{Translation}->{'Could not create the new field.'} = 'Nem sikerült létrehozni az új mezőt.';
-    $Self->{Translation}->{'Need ID!'} = 'Azonosító szükséges!';
+    $Self->{Translation}->{'Need ValidID!'}                            = 'Érvényességazonosító szükséges!';
+    $Self->{Translation}->{'Could not create the new field.'}          = 'Nem sikerült létrehozni az új mezőt.';
+    $Self->{Translation}->{'Need ID!'}                                 = 'Azonosító szükséges!';
     $Self->{Translation}->{'Could not get data for dynamic field %s!'} = 'Nem sikerült lekérni az adatokat a(z) %s dinamikus mezőhöz!';
-    $Self->{Translation}->{'Could not update the field %s!'} = 'Nem sikerült frissíteni a(z) %s mezőt!';
+    $Self->{Translation}->{'Could not update the field %s!'}           = 'Nem sikerült frissíteni a(z) %s mezőt!';
 
     # JS File: Core.Agent.DynamicField.Attachment
     $Self->{Translation}->{'Disable Attachments'} = 'Mellékletek letiltása';
 
     # SysConfig
-    $Self->{Translation}->{'Dynamic Fields Attachment Backend GUI'} = 'Melléklet dinamikus mezők háttérprogram grafikus felhasználói felület';
+    $Self->{Translation}->{'Dynamic Fields Attachment Backend GUI'}       = 'Melléklet dinamikus mezők háttérprogram grafikus felhasználói felület';
     $Self->{Translation}->{'Dynamic Fields Attachment download frontend'} = 'Melléklet dinamikus mezők letöltés előtétprogram';
-    $Self->{Translation}->{'Dynamic field backend registration.'} = 'Dinamikus mező háttérprogram regisztráció.';
-    $Self->{Translation}->{'Dynamic fields extension.'} = 'Dinamikus mező kiterjesztés.';
+    $Self->{Translation}->{'Dynamic field backend registration.'}         = 'Dinamikus mező háttérprogram regisztráció.';
+    $Self->{Translation}->{'Dynamic fields extension.'}                   = 'Dinamikus mező kiterjesztés.';
     $Self->{Translation}->{'Frontend module registration for the admin interface.'} = 'Előtétprogram-modul regisztráció az adminisztrátori felülethez.';
 
-
     push @{ $Self->{JavaScriptStrings} // [] }, (
-    'Cancel',
-    'Delete',
-    'Disable',
-    'Disable Attachments',
+        'Cancel',
+        'Delete',
+        'Disable',
+        'Disable Attachments',
     );
 
 }
